1993f2ba61432e96a658dc1eae59f13215238c18
Yusufmukhtar1981 <73634131+Yusufmukhtar1981@users.noreply.github.com>
2026-07-22 16:20:23 +0000

    Disable R8 for Admin APK

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,8 +1,10 @@
 plugins {
     id("com.android.application")
+
     // START: FlutterFire Configuration
     id("com.google.gms.google-services")
     // END: FlutterFire Configuration
+
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
@@ -18,10 +20,8 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.servicepay_app"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
+
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -30,9 +30,12 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
+            // Use debug signing temporarily so GitHub Actions can build the APK.
             signingConfig = signingConfigs.getByName("debug")
+
+            // Disable R8 shrinking to prevent missing SLF4J classes.
+            isMinifyEnabled = false
+            isShrinkResources = false
         }
     }
 }
