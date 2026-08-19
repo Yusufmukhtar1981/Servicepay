c9ba66f1e35cd3f4f328b331db5117620e5c1506
Yusufmukhtar1981 <73634131+Yusufmukhtar1981@users.noreply.github.com>
2026-08-19 17:35:56 +0000

    Fix admin transaction history endpoint

--- lib/transactions_screen.dart
+++ lib/transactions_screen.dart
@@ -73,7 +73,7 @@ class _TransactionsScreenState extends State<TransactionsScreen> {
       }
 
       final http.Response response = await http.get(
-        Uri.parse('$baseUrl/transactions'),
+        Uri.parse('$baseUrl/admin/transactions'),
         headers: {
           'Accept': 'application/json',
           'Authorization': 'Bearer $token',
